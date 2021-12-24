43540de592458aaa77ef084dd869dc3c17b7a4bf
jameschun7 <chc3484@gmail.com>
2021-12-24 17:48:11 +0900

    [add] demo project

---
+++ Assets/UniversalSDK/Scripts/UniversalSDKSettings.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+namespace Universal.UniversalSDK
+{
+    public class UniversalSDKSettings : ScriptableObject
+    {
+        public const string settingsAssetName = "UniversalSDKSettings";
+        public const string settingsPath = "UniversalSDK/Resources";
+        public const string settingsAssetExtension = ".asset";
+
+        private static UniversalSDKSettings instance;
+
+        public static UniversalSDKSettings Instance
+        {
+            get
+            {
+                if (ReferenceEquals(instance, null))
+                {
+                    instance = Resources.Load(settingsAssetName) as UniversalSDKSettings;
+                    if (ReferenceEquals(instance, null))
+                    {
+                        instance = CreateInstance<UniversalSDKSettings>();
+                    }
+                }
+                return instance;
+            }
+        }
+        
+        //[SerializeField]
+        //private bool devBuild = true;
+
+        [SerializeField]
+        private bool iOSAppleLogin = false;
+        [SerializeField]
+        private bool iOSFacebookLogin = false;
+        [SerializeField]
+        private bool iOSGoogleLogin = false;
+
+        [SerializeField]
+        private string iOSFacebookAppID = "";
+        [SerializeField]
+        private string iOSGoogleClientID = "";
+        [SerializeField]
+        private string iOSReversedClientID = "";        
+
+        //public static bool DevBuild
+        //{
+        //    get { return Instance.devBuild; }
+        //    set { Instance.devBuild = value; }
+        //}
+
+        public static bool UseAppleLogin
+        {
+            get { return Instance.iOSAppleLogin; }
+            set { Instance.iOSAppleLogin = value; }
+        }
+
+        public static bool UseFacebookLogin
+        {
+            get { return Instance.iOSFacebookLogin; }
+            set { Instance.iOSFacebookLogin = value; }
+        }
+
+        public static bool UseGoogleLogin
+        {
+            get { return Instance.iOSGoogleLogin; }
+            set { Instance.iOSGoogleLogin = value; }
+        }
+
+        public static string FacebookAppID
+        {
+            get { return Instance.iOSFacebookAppID; }
+            set { Instance.iOSFacebookAppID = value; }
+        }
+
+        public static string GoogleClientID
+        {
+            get { return Instance.iOSGoogleClientID; }
+            set { Instance.iOSGoogleClientID = value; }
+        }
+
+        public static string ReversedClientID
+        {
+            get { return Instance.iOSReversedClientID; }
+            set { Instance.iOSReversedClientID = value; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# universal-sdk-unity-demo
+## Build Setting
+
+![](https://github.com/jameschun7/universal-sdk-unity2020-demo/blob/main/img/plugins-move.png?raw=true)
+
+Move the Plugins Folder from `Assets/UniversalSDK/Plugins` to `Assets/Plugins`.
+
+
+## Screenshots
+<img src="img/login-view.jpg" alt="App screenshot - login view" width="270" /> <img src="img/google-login.jpg" alt="App screenshot - google login" width="270" /> <img src="img/facebook-login.jpg" alt="App screenshot - facebook login" width="270" />
+<img src="img/main-view.jpg" alt="App screenshot - main view" width="270" /> <img src="img/inapp-purchase.jpg" alt="App screenshot - inapp purchase" width="270" />
+
+## Google InApp Billing TEST
+
+If the message `This item cannot be requested` appears during the in-app purchase test, you must install the tester version released as a test track through the test participation method.
+
+Because the apk built and distributed directly by the developer is not signed through the Google Play Console, test payment may not be smooth.
+
+## Google Login Certificate Thumbprint Settings
+
+If the error code `10` appears when trying to Google Login, you need to check whether the certificate fingerprint is registered in the firebase console.
+
+Register the SHA-1 certificate thumbprint of each app signing key certificate and upload key certificate in Google Play Console App Settings > App Integrity tab in Firebase Console > Project settings > General tab.
+
+![firebase-cert-setting](https://user-images.githubusercontent.com/20632507/147073695-02faf079-66b3-4af8-98e4-5c907a68190f.png)
+
+To enable Google login for both the version released on the Google Play Console and the version directly distributed by the developer with the same certificate, you must register both the upload key certificate and the app signing certificate fingerprint.
